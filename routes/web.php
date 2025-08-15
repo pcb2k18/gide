fddcdade2ae6383f05bd9861f132a9ee27ea0bb9
pcb2k18 <francisnuako@gmail.com>
2025-08-15 04:57:55 +0000

    slugs fixed

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,18 @@ use App\Http\Controllers\HomepageController; // <-- Make sure this is imported
 */
 
 // --- Biography Pages ---
-Route::get('/index.php/{slug}', [BiographyController::class, 'show'])
+Route::get('/de/index.php/{slug}', [BiographyController::class, 'show'])
     ->where('slug', '[a-zA-Z0-9\-]+')
     ->name('biography.show');
 
 // --- Guest Post Pages ---
-Route::get('/index.php/post/{slug}', [PostController::class, 'show'])
+Route::get('/de/index.php/post/{slug}', [PostController::class, 'show'])
     ->name('post.show');
 
 
 // ########## START: CORRECTED HOMEPAGE ROUTE ##########
-// This will now handle the homepage: 
-Route::get('', [HomepageController::class, 'index'])->name('de.home');
+// This will now handle the homepage: http://gi-de.test/de
+Route::get('/de', [HomepageController::class, 'index'])->name('de.home');
 // ########## END: CORRECTED HOMEPAGE ROUTE ##########
 
 
