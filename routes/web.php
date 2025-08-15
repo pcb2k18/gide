14dec8d38d0a25b5dafba0f110b0c42728db3733
pcb2k18 <francisnuako@gmail.com>
2025-08-15 00:28:32 +0000

    Admin fixes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BiographyController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
